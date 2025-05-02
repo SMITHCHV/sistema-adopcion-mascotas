898a2548f53a12f4b26bebf883eadd34e4d769ff
Smith <waltersmithchv@gmail.com>
2025-05-02 08:00:45 -0500

    Migraciones aplicadas y cadena de conexión actualizada

--- obj/Debug/net9.0/SistemaAdopcionMascotas.AssemblyInfo.cs
+++ obj/Debug/net9.0/SistemaAdopcionMascotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaAdopcionMascotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4eb74748b650201eb35e1c00c9d0b6eef6f310")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfbc1d468e47fc0835c1e28ad05e433aa24f6b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaAdopcionMascotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaAdopcionMascotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
